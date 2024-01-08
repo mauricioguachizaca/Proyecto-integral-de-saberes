c9cda5eb0179c96607657f069d5ad22ad331b3c3
Mauricio Guachizaca <149254767+mauricioguachizaca@users.noreply.github.com>
2024-01-08 08:30:25 -0500

    agregaciones

--- frontend/FRONTEND/src/componentes/inicio.jsx
+++ frontend/FRONTEND/src/componentes/inicio.jsx
@@ -5,9 +5,12 @@ import GitHubIcon from '@material-ui/icons/GitHub';
 export const Inicio = () => {
   return (
     <div>
-      <nav className='bg-cyan-700 p-5 flex items-center justify-between fixed top-0 w-full -z-50 shadow-2xl'>
+      <nav className='bg-cyan-700 p-5 flex items-center justify-between fixed top-0 w-full -z-50 shadow-xl'>
+        <div>
+          <Link to="/"  className='text-deep-purple-900 font-bold text-2xl'>CLIK WED</Link>
+        </div>
         <div className='flex items-center space-x-4 ml-auto'>
-          <Link to="/" className="text-white transition duration-300 hover:font-bold hover:shadow-md">Inicio</Link>
+          <Link to="/" className="text-white transition duration-300 hover:font-bold hover:shadow-md fon">Inicio</Link>
           <Link className='text-white transition duration-75 hover:font-bold hover:shadow-md'>Conoce sobre nosotros</Link>
           <Link to="https://github.com/mauricioguachizaca/Proyecto-integral-de-saberes.github.io" className="text-white">
             <GitHubIcon style={{ color: 'black'[500] }} fontSize="large" />
