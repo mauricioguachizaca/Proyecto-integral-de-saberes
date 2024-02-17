4b1f64137045627d806d2cf9cf1c6d19fbe95d38
Fermín Encalada <jose.f.encalada.l@unl.edu.ec>
2024-02-17 13:47:42 -0500

    mejora.jsx

--- frontend/FRONTEND/src/componentes/login.jsx
+++ frontend/FRONTEND/src/componentes/login.jsx
@@ -11,33 +11,34 @@ function Login() {
   });
 
   return (
-    <div className="flex justify-center items-center h-screen bg-blue-100">
-      <div className='bg-teal-700 max-w-md p-10 rounded-md'>
-        <form onSubmit={onSubmit}>
-          <div className="mb-4">
-            <label htmlFor="nombreusuario" className="block text-black text-sm font-bold mb-2">Nombre de usuario:</label>
-            <input
-              type="text"
-              {...register("nombreusuario", { required: true })}
-              className='w-full bg-cyan-400 text-black px-4 py-2 rounded-md my-2'
-            />
-            {errors.nombreusuario && (<p className='text-red-900'>Nombre de usuario es requerido</p>)}
+    <div className="flex justify-center items-center h-screen bg-[#a2e3f9]">
+      <div className='bg-white max-w-sm p-6 rounded-md border border-[#478b6d]'>
+        <form onSubmit={onSubmit} className="grid grid-cols-1 gap-6">
+          <div>
+            <div className="mb-3">
+              <label htmlFor="nombreusuario" className="block text-black text-sm font-bold mb-2">Nombre de usuario:</label>
+              <input
+                type="text"
+                {...register("nombreusuario", { required: true })}
+                className='w-full border border-[#478b6d] text-black px-4 py-2 rounded-md'
+              />
+              {errors.nombreusuario && (<p className='text-red-900'>Nombre de usuario es requerido</p>)}
+            </div>
+            <div className="mb-3">
+              <label htmlFor="password" className="block text-black text-sm font-bold mb-2">Contraseña:</label>
+              <input
+                type="password"
+                {...register("password", { required: true })}
+                className='w-full border border-[#478b6d] text-black px-4 py-2 rounded-md'
+              />
+              {errors.password && (<p className='text-red-900'>La contraseña es requerida</p>)}
+            </div>
           </div>
-          <div className="mb-4">
-            <label htmlFor="password" className="block text-black text-sm font-bold mb-2">Contraseña:</label>
-            <input
-              type="password"
-              {...register("password", { required: true })}
-              className='w-full bg-cyan-400 text-black px-4 py-2 rounded-md my-2'
-            />
-            {errors.password && (<p className='text-red-900'>La contraseña es requerida</p>)}
-          </div>
-
-          <button type="submit" className="bg-blue-500 text-white py-2 px-4 rounded w-full hover:bg-blue-600"> {/* Modificado para ocupar todo el ancho disponible */}
+          <button type="submit" className="bg-[#478b6d] text-white py-2 px-4 rounded hover:bg-blue-600 w-full">
             Iniciar Sesión
           </button>
         </form>
-        <p className="flex gap-x-2 justify-between">
+        <p className="flex gap-x-2 justify-between mt-4">
           ¿No tienes una cuenta aún? <Link to="/registro" className="text-blue-500">Regístrate</Link>
         </p>
       </div>
