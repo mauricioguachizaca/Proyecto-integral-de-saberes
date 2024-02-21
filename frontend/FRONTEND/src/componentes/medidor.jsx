ac0b996684b00156a7f957e9fd5b65c20e8bec6d
Mauricio Guachizaca <149254767+mauricioguachizaca@users.noreply.github.com>
2024-02-21 16:49:25 -0500

    Revert "creacion de los datos para establecer dispositivos"
    This reverts commit 1dc8de96cdedd3e70f1e19f2f8f752f984c006ea.

--- frontend/FRONTEND/src/componentes/medidor.jsx
+++ frontend/FRONTEND/src/componentes/medidor.jsx
@@ -1,9 +1,18 @@
+<<<<<<< HEAD
 import React from 'react'
 
 export const Medidor = () => {
+=======
+
+function Medidor() {
+>>>>>>> parent of 1dc8de9 (creacion de los datos para establecer dispositivos)
   return (
     <div>
       <div className='bg-purple-700'>hola que haces </div> 
       </div>
   )
 }
+<<<<<<< HEAD
+=======
+export default Medidor;
+>>>>>>> parent of 1dc8de9 (creacion de los datos para establecer dispositivos)
