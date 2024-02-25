dc4bd226192578230721dab9ef470578164badee
Letty Cañar <148287189+LettyYas@users.noreply.github.com>
2024-02-25 10:01:02 -0500

    Update informacion.jsx

--- frontend/FRONTEND/src/componentes/informacion.jsx
+++ frontend/FRONTEND/src/componentes/informacion.jsx
@@ -2,21 +2,17 @@ import React, { useState } from 'react';
 import FlujoTrabajo from './imagenes/FlujoDeTrabajoPis.png';
 import DiagramaClases from './imagenes/Diagrama.jpg';
 
-
 export const Informacion = ({ modoNoche }) => {
   const [flujoAmpliada, setFlujoAmpliada] = useState(false);
-  const [DiagramaAmpliada, setDiagramaAmpliada] = useState(false);
-
+  const [diagramaAmpliada, setDiagramaAmpliada] = useState(false);
 
   const toggleFlujoAmpliada = () => {
     setFlujoAmpliada(!flujoAmpliada);
   };
 
-
   const toggleDiagramaAmpliada = () => {
-    setDiagramaAmpliada(!DiagramaAmpliada);
+    setDiagramaAmpliada(!diagramaAmpliada);
   };
-
 
   return (
     <div className={`min-h-screen flex flex-col items-center justify-center ${modoNoche ? 'bg-[#1b1a1b]' : 'bg-gray-200'}`}>
@@ -29,9 +25,9 @@ export const Informacion = ({ modoNoche }) => {
           <p className="mb-4"></p>
           {flujoAmpliada && (
             <div
-              className="fixed top-0 left-0 right-0 bottom-0 z-50 overflow-hidden flex items-center justify-center"
-              style={{ background: 'rgba(255, 255, 255, 0.9)' }}
-              onClick={toggleFlujoAmpliada} >
+              className={`fixed top-0 left-0 right-0 bottom-0 z-50 overflow-hidden flex items-center justify-center ${modoNoche ? 'bg-[#1b1a1b]' : 'bg-gray-200'}`}
+              onClick={toggleFlujoAmpliada}
+            >
               <img
                 src={FlujoTrabajo}
                 alt="Descripción de la imagen"
@@ -47,11 +43,9 @@ export const Informacion = ({ modoNoche }) => {
             onClick={toggleFlujoAmpliada}
           />
 
-
-          {DiagramaAmpliada && (
+          {diagramaAmpliada && (
             <div
-              className="fixed top-0 left-0 right-0 bottom-0 z-50 overflow-hidden flex items-center justify-center"
-              style={{ background: 'rgba(255, 255, 255, 0.9)' }}
+              className={`fixed top-0 left-0 right-0 bottom-0 z-50 overflow-hidden flex items-center justify-center ${modoNoche ? 'bg-[#1b1a1b]' : 'bg-gray-200'}`}
               onClick={toggleDiagramaAmpliada}
             >
               <img
@@ -65,17 +59,17 @@ export const Informacion = ({ modoNoche }) => {
           <img
             src={DiagramaClases}
             alt="Descripción de la nueva imagen"
-            className={`w-full h-auto rounded-md cursor-pointer ${DiagramaAmpliada ? 'filter brightness-50' : ''} mb-4 mx-auto`}
+            className={`w-full h-auto rounded-md cursor-pointer ${diagramaAmpliada ? 'filter brightness-50' : ''} mb-4 mx-auto`}
             style={{ maxWidth: 'calc(100% + 20px)' }}
             onClick={toggleDiagramaAmpliada}
           />
-
-
         </div>
       </div>
     </div>
   );
 };
+
+
 
 
 
