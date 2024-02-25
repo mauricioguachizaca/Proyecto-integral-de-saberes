9e6a94612df0b9f62bed7fc46cc710f413fc522b
Borisir <148477108+Borisir@users.noreply.github.com>
2024-02-25 17:27:22 -0500

    graficos mostrar.jsx

--- frontend/FRONTEND/src/componentes/mostrar.jsx
+++ frontend/FRONTEND/src/componentes/mostrar.jsx
@@ -16,26 +16,31 @@ function Mostrar() {
   const isLoading = dispositivoMasConsumo === null || consumoTotal === null;
 
   return (
-    <div className="flex flex-col items-center justify-center mb-16">
-      <div className="mb-9">Mis gráficas</div>
-      <div className="flex flex-col items-center mb-8 h-60">
-        <CharComponent />
-        <Pastel height={200} />
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br bg-[#a2e3f9]  text-black">
+      <div className="text-5xl font-bold mb-8 text-center">¡Tus Datos Energéticos!</div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div className="flex flex-col items-center p-4 bg-white opacity-70 rounded-xl backdrop-blur-md shadow-md">
+          <div className="text-3xl font-bold mb-4">Visualización Gráfica</div>
+          <CharComponent />
+        </div>
+        <div className="flex flex-col items-center p-4 bg-white opacity-70 rounded-xl backdrop-blur-md shadow-md">
+          <div className="text-3xl font-bold mb-4">Distribución de Consumo</div>
+          <Pastel height={200} />
+        </div>
       </div>
-      <div className="bg-gray-100 p-8 rounded-lg text-center" style={{ marginTop: '900px' }}>
+      <div className="bg-white opacity-70 p-8 rounded-lg text-center mt-8 max-w-md backdrop-blur-md">
         {isLoading ? (
-          <p>Cargando...</p>
+          <p className="text-lg">Cargando...</p>
         ) : (
           <div>
-            <p>Mi dispositivo que más consume es: {dispositivoMasConsumo.nombreDispositivo}</p>
-            <p>El consumo total de energía al mes es: {consumoTotal} W</p>
+            <p className="text-xl mb-4">Tu dispositivo que más consume es: {dispositivoMasConsumo.nombreDispositivo}</p>
+            <p className="text-xl">El consumo total de energía al mes es: {consumoTotal} W</p>
           </div>
         )}
       </div>
       <button
         type="submit"
-        className="col-span-2 bg-[#478b6d] text-white py-2 px-4 rounded hover:bg-[#5d8dee]"
-        style={{ marginTop: '1cm' }}
+        className="bg-[#478b6d] text-white py-2 px-4 rounded hover:bg-[#5d8dee]"
       >
         <Link to="/medidor">Regresar</Link>
       </button>
