4a6aefc7ced3f704c1998ce15e0bc7ff31d370b1
Letty Cañar <148287189+LettyYas@users.noreply.github.com>
2024-02-25 20:23:00 -0500

    Modo Oscuro

--- frontend/FRONTEND/src/componentes/medidor.jsx
+++ frontend/FRONTEND/src/componentes/medidor.jsx
@@ -7,8 +7,8 @@ function Medidor() {
   const { cerrar } = useAuth();
 
   return (
-    <div className="bg-blue-gray-50 flex flex-col min-h-screen">
-      <nav className={`bg-[#478b6d] p-6 flex flex-col sm:flex-row items-center justify-between fixed w-full z-50 shadow-xl`}>
+    <div className="bg-blue-gray-50 dark:bg-[#1c201e] flex flex-col min-h-screen">
+      <nav className={`bg-[#478b6d] dark:bg-[#17301a] p-6 flex flex-col sm:flex-row items-center justify-between fixed w-full z-50 shadow-xl`}>
         <div className="flex items-center mb-1 sm:ml-4">
           <Link to="/" className="text-white font-extrabold text-2xl">CLIK WED</Link>
         </div>
@@ -20,12 +20,12 @@ function Medidor() {
       </nav>
 
       <div className="flex justify-between items-center ml-6 mt-24 mb-4 mr-6"> 
-        <h1 className="text-2xl">Mis dispositivos</h1>
+        <h1 className="text-2xl dark:text-white">Mis dispositivos </h1>
         <div className="flex space-x-4"> {/* Contenedor para los botones */}
           <button className="bg-teal-700 text-white py-2 px-4 rounded hover:bg-teal-600">
             <Link to="/agregarmedidor">Agregar dispositivo</Link>
           </button>
-          <button className="bg-[#a2e3f9] text-black py-2 px-4 rounded hover:bg-[#53ccf1]">
+          <button className="bg-[#a2e3f9] dark:bg-[#1e2f50] text-black dark:text-white py-2 px-4 rounded hover:bg-[#53ccf1] ">
             <Link to="/mostrar">Calcular mi consumo</Link>
           </button>
         </div>
