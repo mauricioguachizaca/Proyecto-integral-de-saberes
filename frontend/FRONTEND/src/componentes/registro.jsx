df222c43686e488936382d8742f226a611c281f1
Letty Cañar <148287189+LettyYas@users.noreply.github.com>
2024-02-29 16:36:56 -0500

    Cambios en contraseña

--- frontend/FRONTEND/src/componentes/registro.jsx
+++ frontend/FRONTEND/src/componentes/registro.jsx
@@ -1,6 +1,6 @@
+import React, { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { useAuth } from '../context/AuthContext';
-import { useEffect, useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import GitHubIcon from '@material-ui/icons/GitHub';
 
@@ -14,8 +14,16 @@ const Registro = ({ modoNoche }) => {
     if (isAuthenticated) navigation("/medidor");
   }, [isAuthenticated]);
 
+  const validatePassword = (value) => {
+    const regex = /^(?=.*[A-Z])(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/;
+    return regex.test(value);
+  };
+
   const onSubmit = handleSubmit(async (values) => {
-    console.log(values);
+    if (!validatePassword(values.password)) {
+      alert("La contraseña debe tener al menos 8 caracteres, una letra mayúscula y un carácter especial.");
+      return;
+    }
     validarcredenciales(values);
   });
 
@@ -37,95 +45,96 @@ const Registro = ({ modoNoche }) => {
           </a>
         </div>
       </nav>
-    <div className={`${modoNoche ? 'bg-[#1c201e]' : 'bg-[#a2e3f9]'} flex justify-center items-center h-screen`}>
-      <div className={`${modoNoche ? 'bg-gray-800' : 'bg-white'}  max-w-4xl p-10 rounded-md border border-[#478b6d]`}>
-      <h1 className={`${modoNoche ? 'text-white' : 'text-black'} text-3xl font-bold mb-6 text-center`}>Registro de Usuarios</h1>
-        <form onSubmit={onSubmit} className="grid grid-cols-2 gap-6">
-          <div>
-            <div className="mb-6">
-              <label htmlFor="nombre" className={`block ${modoNoche ? 'text-white' : 'text-black'} text-sm font-bold mb-2`}>Nombres:</label>
-              <input
-                type="text"
-                {...register("nombre", { required: true })}
-                className='w-full border border-[#478b6d] text-black bg-[#d4e8ee] px-4 py-2 rounded-md '
-              />
-              {errors.nombre && (<p className='text-red-900'>Nombres es requerido</p>)}
-            </div>
-            <div className="mb-6">
-              <label htmlFor="apellido" className={`block ${modoNoche ? 'text-white' : 'text-black'} text-sm font-bold mb-2`}>Apellidos:</label>
-              <input
-                type="text"
-                {...register("apellido", { required: true })}
-                className='w-full border border-[#478b6d] text-black bg-[#d4e8ee] px-4 py-2 rounded-md'
-              />
-              {errors.apellido && (<p className='text-red-900'>Apellidos es requerido</p>)}
-            </div>
-            <div className="mb-6">
-              <label htmlFor="cedula" className={`block ${modoNoche ? 'text-white' : 'text-black'} text-sm font-bold mb-2`}>Cédula:</label>
-              <input
-                type="text"
-                {...register("cedula", { required: true })}
-                className='w-full border border-[#478b6d] text-black bg-[#d4e8ee] px-4 py-2 rounded-md'
-              />
-              {errors.cedula && (<p className='text-red-900'>Cédula es requerido</p>)}
-            </div>
-            <div className="mb-6">
-              <label htmlFor="nombreusuario" className={`block ${modoNoche ? 'text-white' : 'text-black'} text-sm font-bold mb-2`}>Nombre de usuario:</label>
-              <input
-                type="text"
-                {...register("nombreusuario", { required: true })}
-                className='w-full border border-[#478b6d] text-black bg-[#d4e8ee] px-4 py-2 rounded-md'
-              />
-              {errors.nombreusuario && (<p className='text-red-900'>Nombre de usuario es requerido</p>)}
-            </div>
-          </div>
-          <div>
-            <div className="mb-6">
-              <label htmlFor="provincia" className={`block ${modoNoche ? 'text-white' : 'text-black'} text-sm font-bold mb-2`}>Provincia:</label>
-              <input
-                type="text"
-                {...register("provincia", { required: true })}
-                className='w-full border border-[#478b6d] text-black bg-[#d4e8ee] px-4 py-2 rounded-md'
-              />
-              {errors.provincia && (<p className='text-red-900'>Provincia es requerida</p>)}
-            </div>
-            <div className="mb-6">
-              <label htmlFor="password" className={`block ${modoNoche ? 'text-white' : 'text-black'} text-sm font-bold mb-2`}>Contraseña:</label>
-              <div className="relative">
+      <div className={`${modoNoche ? 'bg-[#1c201e]' : 'bg-[#a2e3f9]'} flex justify-center items-center h-screen`}>
+        <div className={`${modoNoche ? 'bg-gray-800' : 'bg-white'}  max-w-4xl p-10 rounded-md border border-[#478b6d]`}>
+          <h1 className={`${modoNoche ? 'text-white' : 'text-black'} text-3xl font-bold mb-6 text-center`}>Registro de Usuarios</h1>
+          <form onSubmit={onSubmit} className="grid grid-cols-2 gap-6">
+            <div>
+              <div className="mb-6">
+                <label htmlFor="nombre" className={`block ${modoNoche ? 'text-white' : 'text-black'} text-sm font-bold mb-2`}>Nombres:</label>
                 <input
-                  type={showPassword ? "text" : "password"}
-                  {...register("password", { required: true })}
+                  type="text"
+                  {...register("nombre", { required: true })}
+                  className='w-full border border-[#478b6d] text-black bg-[#d4e8ee] px-4 py-2 rounded-md '
+                />
+                {errors.nombre && (<p className='text-red-900'>Nombres es requerido</p>)}
+              </div>
+              <div className="mb-6">
+                <label htmlFor="apellido" className={`block ${modoNoche ? 'text-white' : 'text-black'} text-sm font-bold mb-2`}>Apellidos:</label>
+                <input
+                  type="text"
+                  {...register("apellido", { required: true })}
                   className='w-full border border-[#478b6d] text-black bg-[#d4e8ee] px-4 py-2 rounded-md'
                 />
-                <button
-                  type="button"
-                  onClick={() => setShowPassword(!showPassword)}
-                  className="absolute inset-y-0 right-0 flex items-center px-3 focus:outline-none"
-                >
-                  {showPassword ? "Ocultar" : "Mostrar"}
-                </button>
+                {errors.apellido && (<p className='text-red-900'>Apellidos es requerido</p>)}
               </div>
-              {errors.password && (<p className='text-red-900'>La contraseña es requerida</p>)}
+              <div className="mb-6">
+                <label htmlFor="cedula" className={`block ${modoNoche ? 'text-white' : 'text-black'} text-sm font-bold mb-2`}>Cédula:</label>
+                <input
+                  type="text"
+                  {...register("cedula", { required: true })}
+                  className='w-full border border-[#478b6d] text-black bg-[#d4e8ee] px-4 py-2 rounded-md'
+                />
+                {errors.cedula && (<p className='text-red-900'>Cédula es requerida</p>)}
+              </div>
+              <div className="mb-6">
+                <label htmlFor="nombreusuario" className={`block ${modoNoche ? 'text-white' : 'text-black'} text-sm font-bold mb-2`}>Nombre de usuario:</label>
+                <input
+                  type="text"
+                  {...register("nombreusuario", { required: true })}
+                  className='w-full border border-[#478b6d] text-black bg-[#d4e8ee] px-4 py-2 rounded-md'
+                />
+                {errors.nombreusuario && (<p className='text-red-900'>Nombre de usuario es requerido</p>)}
+              </div>
             </div>
-            <div className="mb-6">
-              <label htmlFor="correo" className={`block ${modoNoche ? 'text-white' : 'text-black'} text-sm font-bold mb-2`}>Correo:</label>
-              <input
-                type="text"
-                {...register("correo", { required: true })}
-                className='w-full border border-[#478b6d] text-black bg-[#d4e8ee] px-4 py-2 rounded-md'
-              />
-              {errors.correo && (<p className='text-red-900'>Correo es requerido</p>)}
+            <div>
+              <div className="mb-6">
+                <label htmlFor="provincia" className={`block ${modoNoche ? 'text-white' : 'text-black'} text-sm font-bold mb-2`}>Provincia:</label>
+                <input
+                  type="text"
+                  {...register("provincia", { required: true })}
+                  className='w-full border border-[#478b6d] text-black bg-[#d4e8ee] px-4 py-2 rounded-md'
+                />
+                {errors.provincia && (<p className='text-red-900'>Provincia es requerida</p>)}
+              </div>
+              <div className="mb-6">
+                <label htmlFor="password" className={`block ${modoNoche ? 'text-white' : 'text-black'} text-sm font-bold mb-2`}>Contraseña:</label>
+                <div className="relative">
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    {...register("password", { required: true })}
+                    className='w-full border border-[#478b6d] text-black bg-[#d4e8ee] px-4 py-2 rounded-md'
+                  />
+                  <button
+                    type="button"
+                    onClick={() => setShowPassword(!showPassword)}
+                    className="absolute inset-y-0 right-0 flex items-center px-3 focus:outline-none"
+                  >
+                    {showPassword ? "Ocultar" : "Mostrar"}
+                  </button>
+                </div>
+                {errors.password && (<p className='text-red-900'>La contraseña es requerida</p>)}
+                {errors.password && errors.password.type === "validate" && (<p className='text-red-900'>La contraseña debe tener al menos 8 caracteres, una letra mayúscula y un carácter especial.</p>)}
+              </div>
+              <div className="mb-6">
+                <label htmlFor="correo" className={`block ${modoNoche ? 'text-white' : 'text-black'} text-sm font-bold mb-2`}>Correo:</label>
+                <input
+                  type="text"
+                  {...register("correo", { required: true })}
+                  className='w-full border border-[#478b6d] text-black bg-[#d4e8ee] px-4 py-2 rounded-md'
+                />
+                {errors.correo && (<p className='text-red-900'>Correo es requerido</p>)}
+              </div>
             </div>
-          </div>
-          <button type="submit" className="col-span-2  bg-[#478b6d] text-white py-2 px-4 rounded hover:bg-[#5d8dee]">
-            REGISTRO
-          </button>
-        </form>
-        <p className={`${modoNoche ? 'text-white' : 'text-black'} flex items-center justify-between mt-4`}>
-          ¿Si ya tienes una cuenta? <Link to="/iniciar" className={`font-bold ${modoNoche ? 'text-[#84dcec]' : 'text-[#47728b]'}`}>Iniciar Sesión</Link>
-        </p>
+            <button type="submit" className="col-span-2  bg-[#478b6d] text-white py-2 px-4 rounded hover:bg-[#5d8dee]">
+              REGISTRO
+            </button>
+          </form>
+          <p className={`${modoNoche ? 'text-white' : 'text-black'} flex items-center justify-between mt-4`}>
+            ¿Si ya tienes una cuenta? <Link to="/iniciar" className={`font-bold ${modoNoche ? 'text-[#84dcec]' : 'text-[#47728b]'}`}>Iniciar Sesión</Link>
+          </p>
+        </div>
       </div>
-    </div>
     </div>
   );
 }
